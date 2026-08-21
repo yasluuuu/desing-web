2a581f4c09aeacfea36c55b766031dc8bbf5ff13
yasluuuu <yasmin.lucia@escolar.ifrn.edu.br>
2026-08-21 13:26:21 -0300

    fiz o js

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,16 @@
 alert("hello word!");
+
+var nasc=2010
+let nome = "yayas"
+const viva = true;
+let altura  = 1.66;
+
+if(viva){
+    let saudacao = "olá" +"!";
+    let msg = `altura ${altura} m | idade ${2026-nasc}`;
+    alert(saudacao+"\n"+ msg);
+    }
+
+    else{
+    alert("R.I.P");
+}
