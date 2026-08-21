7a868d25cabed3e55423c0d248c79306e0901159
yasluuuu <yasmin.lucia@escolar.ifrn.edu.br>
2026-08-21 14:13:54 -0300

    criacao de variaveis

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var nasc= 2010
+let nome= "yasmim";
+const viva = true;
+
+function calcIdade(ano){
+   let idade = ano - nasc;
+   alert (`dentro de função - idade ${idade}`);
+   return idade;
+}
+
+calcIdade();
+ alert (`fora de função - idade ${idade}`);
+ alert (`fora de função : chamando caucIdade ${calcIdade(2027)}`);
