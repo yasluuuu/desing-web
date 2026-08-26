a38f4ef479a51365522c25f6a425ff353be8924c
yasluuuu <yasmin.lucia@escolar.ifrn.edu.br>
2026-08-26 17:53:51 -0300

    salveii

---
+++ js/app3.js
@@ -0,0 +1,15 @@
+let nasc = 2009;
+let nome =" Dudah";
+let viva = true;
+let altura = 1.62;
+let sexo;
+let peso = null;
+let imc = 'peso'*10;
+
+alert(`ano de nascimento - tipo: ${typeof(nasc)}`)
+alert(`nome - tipo: ${typeof(nome)}`)
+alert(`Viva - tipo: ${typeof(viva)}`)
+alert(`Altura- tipo: ${typeof(altura)}`)
+alert(`Sexo - tipo: ${typeof(sexo)}`)
+alert(`Peso - tipo: ${typeof(peso)}`)
+alert(`IMC - tipo: ${typeof(imc)}`)
