c716cabc19c24cde8f773f44e3d49d9ee9b0f778
yasluuuu <yasmin.lucia@escolar.ifrn.edu.br>
2026-08-26 17:12:04 -0300

    beber

--- js/app.js
+++ js/app.js
@@ -8,10 +8,13 @@ function calcIdade(ano=2026){
    if (idade<18)
      {
        menor = true;
+       var podebeber=false;
+
      } else{
        menor = false;
+       var podebeber = true
      }
-     alert(`${nome} é  menor de idade? ${menor}`)
+     alert(`${nome} é  menor de idade? ${menor}\nidade: ${idade}\npode beber: ${podebeber}`)
      return idade;
 }
 
