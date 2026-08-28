ed63a7e4885e00c655d7f72d670272a514cdbeb7
yasluuuu <yasmin.lucia@escolar.ifrn.edu.br>
2026-08-28 14:03:28 -0300

    CORRIGI

--- js/app2.js
+++ js/app2.js
@@ -1,11 +1,11 @@
 let  n1 = prompt*("digite um numero ");
-n1 = Number(n1)
-let n2 = prompt("digite outro nome ");
-n2 = Number(n2)
+n1 = Number(n1);
+let n2 = prompt("digite outro numero ");
+n2 = Number(n2);
 alert(`${n1} + ${n2} = ${n1+n2}`);
-alert(`${n1} + ${n2} = ${n1-n2}`);
-alert(`${n1} + ${n2} = ${n1/n2}`);
-alert(`${n1} + ${n2} = ${n1%n2}`);
-alert(`${n1} + ${n2} = ${n1*n2}`);
-alert(`${n1} + ${n2} = ${n1**n2}`);
+alert(`${n1} - ${n2} = ${n1-n2}`);
+alert(`${n1} * ${n2} = ${n1/n2}`);
+alert(`${n1} / ${n2} = ${n1%n2}`);
+alert(`${n1} % ${n2} = ${n1*n2}`);
+alert(`${n1} ** ${n2} = ${n1**n2}`);
 
