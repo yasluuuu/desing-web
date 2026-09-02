8a4a36c03e0282b2b67459062f4efa77332e4d49
yasluuuu <yasmin.lucia@escolar.ifrn.edu.br>
2026-09-02 17:21:50 -0300

    mexi no js

--- js/app1.js
+++ js/app1.js
@@ -1,3 +1,7 @@
-let nasc = Number(prompt("digite seu anode nascimento "))
+let nasc = Number(prompt("digite seu ano de nascimento "))
 let idade = 2026 - nasc;
 alert(`sua idade é ${idade}`);
+
+if (idade >= 18){
+    alert("vc é maior de idade")
+}
