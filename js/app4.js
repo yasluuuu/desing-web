2b86afd213b87b235990d75040696b04727daf69
yasluuuu <yasmin.lucia@escolar.ifrn.edu.br>
2026-09-04 14:27:02 -0300

    vesionei

--- js/app4.js
+++ js/app4.js
@@ -1,4 +1,14 @@
 let vezes = Number(prompt("digite o numero de vezes"));
 for (let i = 1; i<=vezes; i ++){
-    alert(`contei ${i}vezes`)
+    if(vezes > 100){
+        alert("valor invalido");
+        break
+    }
+    alert(`contei ${i}vez`);
+    if (i%2!==0){
+        continue;
+
+    }
+   alert(`${i} é par`);
+
 }
