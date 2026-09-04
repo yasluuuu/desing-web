a592146fd3734e1d183c7544f2070cda8b4da1b9
yasluuuu <yasmin.lucia@escolar.ifrn.edu.br>
2026-09-04 13:25:28 -0300

    salvei

---
+++ js/app3.js
@@ -0,0 +1,16 @@
+let dia = prompt("escolha um dia da semana\nsendo 1:domingo")
+dia= Number(dia);
+switch(dia){
+case 1: alert("vc escolheu domingo"); break;
+case 2: alert("vc escolheu segunda");break;
+case 3: alert("vc escolheu terça"); break;
+case 4: alert("vc escolheu querta"); break;
+case 5: alert("vc escolheu quinta");break;
+case 6: alert("vc escolheu sexta"); break;
+case 7: alert("vc escolheu sabado"); break;
+default: alert("dia invalido");break;
+}
+
+
+
+
