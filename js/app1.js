f0d5a537644b5893af57aa0df0530633787f14c2
yasluuuu <yasmin.lucia@escolar.ifrn.edu.br>
2026-09-18 14:24:18 -0300

    primeiras alterações

---
+++ js/app1.js
@@ -0,0 +1,7 @@
+const amigos = ["pedro","ana","clara"];
+tam = amigos . length
+
+console.log(`tamanho ${amigos}`);
+console.log(`tamanho ${tam}`);
+console.log(`tamanho ${amigos[0]}`);
+console.log(`ultimo ${amigos[-1]}`);
